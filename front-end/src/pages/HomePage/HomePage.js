6edb71508e0342c4d997f66776b39bab424e9b1f
Tony Li <tonyli@Tonys-MacBook-Pro.local>
2021-04-08 20:36:23 +1000

    HomePage update

--- front-end/src/pages/HomePage/HomePage.js
+++ front-end/src/pages/HomePage/HomePage.js
@@ -1,36 +1,63 @@
 import React from 'react';
+import classes from './HomePage.css'
 
 const homePage = () => {
     return(
-        <div className="banner-container">
-            <div className="nav-container">
-                <div className="navbar"> 
-                    <div className="logo">
-                        <img src="images/logo.png" width="125px" alt="store-logo"></img>
+        // ===========================================
+        // This is the header section
+        // ===========================================
+        <div>
+            <div className="banner-container">
+                <div className="nav-container">
+                    <div className="navbar"> 
+                        <div className="logo">
+                            <img src="images/logo.png" width="125px" alt="store-logo"></img>
+                        </div>
+                        <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Products</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Account</a></li>
+                            <li><a href="#"><img src="images/cart.png" width="30px" height="30px" alt="shopping-cart"></img></a></li>
+                        </ul>
+                        </nav>
                     </div>
-                    <nav>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Account</a></li>
-                        <li><a href="#"><img src="images/cart.png" width="30px" height="30px" alt="shopping-cart"></img></a></li>
-                    </ul>
-                    </nav>
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <h1>Give your workout <br></br>a new style!</h1>
+                        <p>Success isn't always about greatness. It's about consistency. consistent <br></br> hard work gains sucess. Greatness will follow!</p>
+                        <a className="banner-btn" href="">Explore now</a>
+                    </div>
+                    <div class="col-2">
+                        <img src="images/image1.png" alt="banner-pic"></img>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-2">
-                    <h1>Give your workout <br></br>a new style!</h1>
-                    <p>Success isn't always about greatness. It's about consistency. consistent <br></br> hard work gains sucess. Greatness will follow!</p>
-                    <a className="banner-btn" href="">Explore now</a>
-                </div>
-                <div class="col-2">
-                    <img src="images/image1.png" alt="banner-pic"></img>
+
+            {/* // ===========================================
+            // This is the categories section
+            // =========================================== */}
+
+            <div className="categories">
+                <div className="categories-wrapper">
+                    <div className="row">
+                        <div className="col-3">
+                            <img src="images/category-1.jpg" alt="category-pic"></img>
+                        </div>
+                        <div className="col-3">
+                            <img src="images/category-2.jpg" alt="category-pic"></img>
+                        </div>
+                        <div className="col-3">
+                            <img src="images/category-3.jpg" alt="category-pic"></img>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        
 
     );
     
